742ef822d538a485f41ed1bf417d11fe36be3a5e
mlwebdev <marvinllbiz@gmail.com>
2019-02-06 21:23:53 -0500

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,16 @@
+
+(function (btn) { 
+  function clearInputs(inputs, i, len, e) {
+    // inputs variable is for the element input and effects all inputs
+    inputs = document.querySelectorAll('input');
+    len = inputs.length;
+    for (i = 0; i < len; i++) {
+        inputs[i].value = '';
+    }
+    if(e) e.preventDefault()
+    console.log("Inputs cleared.");
+  }
+  // btn variable has same name as the button's id
+  btn = document.getElementById("btn"); 
+  btn.addEventListener("click", clearInputs);
+})(); // Clear Inputs
